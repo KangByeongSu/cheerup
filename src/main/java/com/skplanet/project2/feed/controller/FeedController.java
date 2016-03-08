eb127fb64955b6ef1e8fc72cdb1c679235414256
1003391 <katrina09@sk.com>
2016-03-08 14:31:21 +0900

    feedController추가

--- src/main/java/com/skplanet/project2/feed/controller/FeedController.java
+++ src/main/java/com/skplanet/project2/feed/controller/FeedController.java
@@ -1,5 +1,33 @@
 package com.skplanet.project2.feed.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.skplanet.project2.feed.service.FeedService;
+
+@Controller
+@RequestMapping("/feed")
 public class FeedController {
+	
+	@Autowired
+	FeedService feedservice;
+	
+	
+	@RequestMapping(value="/lists")
+	public String getFeedlists(){
+		
+		System.out.println("dd");
+		feedservice.getFeedlists();
+		return "view";
+	}
+	
+	@RequestMapping(value="/add")
+	public String addFeedPost(){
+	
+		return "view";
+	}
+	
+	
 
 }
