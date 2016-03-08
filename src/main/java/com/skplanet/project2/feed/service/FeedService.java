b7c3156e07289b1afc61223618ebb9310c65d402
1003391 <katrina09@sk.com>
2016-03-08 14:33:20 +0900

    feedService commit

---
+++ src/main/java/com/skplanet/project2/feed/service/FeedService.java
@@ -0,0 +1,10 @@
+package com.skplanet.project2.feed.service;
+
+import java.util.List;
+
+
+public interface FeedService {
+	
+	public void getFeedlists();
+
+}
---
+++ src/main/java/com/skplanet/project2/feed/service/FeedServiceImpl.java
@@ -0,0 +1,16 @@
+package com.skplanet.project2.feed.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FeedServiceImpl implements FeedService {
+
+	@Override
+	public void getFeedlists() {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+		return;
+		
+	}
+	
+}
