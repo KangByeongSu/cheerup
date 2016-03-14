4a3ff2aec098cdf148c9ab5fc70394904fe92563
1002718 <sansam@sk.com>
2016-03-14 09:01:36 +0900

    Commit Like DTO delete annotation

--- src/main/java/com/skplanet/project2/model/LikeDTO.java
+++ src/main/java/com/skplanet/project2/model/LikeDTO.java
@@ -30,37 +30,4 @@ public class LikeDTO {
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
-	
-	/*private int l_up;
-	private int l_down;
-	private int elephant_Contents_c_id;
-	private String elephant_User_u_id;
-	public int getL_up() {
-		return l_up;
-	}
-	public void setL_up(int l_up) {
-		this.l_up = l_up;
-	}
-	public int getL_down() {
-		return l_down;
-	}
-	public void setL_down(int l_down) {
-		this.l_down = l_down;
-	}
-	public int getElephant_Contents_c_id() {
-		return elephant_Contents_c_id;
-	}
-	public void setElephant_Contents_c_id(int elephant_Contents_c_id) {
-		this.elephant_Contents_c_id = elephant_Contents_c_id;
-	}
-	public String getElephant_User_u_id() {
-		return elephant_User_u_id;
-	}
-	public void setElephant_User_u_id(String elephant_User_u_id) {
-		this.elephant_User_u_id = elephant_User_u_id;
-	}
-
-	*/
-	
-	
 }
