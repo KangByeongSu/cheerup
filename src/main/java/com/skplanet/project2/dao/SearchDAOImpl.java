934bd0de63131a9b90b79f7e95390c2771691581
1003391 <katrina09@sk.com>
2016-03-15 20:08:39 +0900

    commint_검색 backend api 개발 완료_yoonji

---
+++ src/main/java/com/skplanet/project2/dao/SearchDAOImpl.java
@@ -0,0 +1,36 @@
+package com.skplanet.project2.dao;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.skplanet.project2.model.SearchDTO;
+
+@Repository
+public class SearchDAOImpl implements SearchDAO{
+
+	@Autowired
+	private SqlSession sqlSession;
+	
+	
+	@Override
+	public List<SearchDTO> selectinterestList(String hashtag, int pageNum, String type) {
+		// TODO Auto-generated method stub
+		List<SearchDTO> searchlist = new ArrayList<SearchDTO>();
+		Map<String,Object> map = new HashMap<String,Object>();
+		map.put("hashtag", hashtag);
+		map.put("pageNum", pageNum);
+		map.put("type", type);
+		searchlist = sqlSession.selectList("search.selectInterestItem",map);
+		
+		return searchlist;
+	}
+
+	
+
+}
