58141cefc88b5f211193674852cd0770d94593fa
1002723 <donguk@sk.com>
2016-03-16 14:31:28 +0900

    msg modify

--- src/main/webapp/resources/js/join.js
+++ src/main/webapp/resources/js/join.js
@@ -43,15 +43,15 @@ $(document).ready(function() {
 	                remote : resultText
 	            },*/
 	        	uPw: {
-	                required: "패스워드를 입력해줘",
-	                minlength: "5자리 이상이야"
+	                required: "패스워드를 입력해주세요",
+	                minlength: "비밀번호는 5자리 이상입니다"
 	            },
 	            password_again:{
-	            	required: "패스워드 확인해줘",
-	                minlength: "5자리 이상이야",
-                    equalTo: "패스워드가 달라ㅠ"	
+	            	required: "패스워드 확인해주세요",
+	                minlength: "비밀번호는 5자리 이상입니다",
+                    equalTo: "비밀번호가 서로 다릅니다"	
                 },
-	            uNickname: "닉네임을 입력해줘"
+	            uNickname: "닉네임을 입력해주세요"
 	        },
 	        submitHandler: function(form) {
 	        	if(boolcheckid==0)
@@ -70,13 +70,15 @@ $(document).ready(function() {
 					method : "GET",
 					success : function(resData) {
 						$("#checkidtext").html(resData.msg);
+						$("#checkidtext").show();
 						boolcheckid = resData.isSuccess;
 					},
 					error : function() {
 					}
 				});
 		} else{
-			$("#checkidtext").html("5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능해.");
+			$("#checkidtext").html("5~20자의 영문 소문자, 숫자와 특수기호만 사용 가능합니다");
+			$("#checkidtext").show();
 			boolcheckid = 1;
 
 		}
